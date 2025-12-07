f14c933198c58f9884cd3228064635b5fc15ac52
RazorFlux <105893554+RazorFlux@users.noreply.github.com>
2025-12-07 15:00:58 -0800

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,12 @@ public final class Constants {
     public static class IntakeConstants {
         // CAN ID for the intake motor
         // TODO: Set this to the actual CAN ID of your intake motor
-        public static final int INTAKE_MOTOR_ID = 16;
+        public static final int INTAKE_MOTOR_ID = 27;
+        public static final int INDEXER_MOTOR_ID = 29;
     }
+
+    public static class ShooterConstants {
+        public static final int SHOOT_MOTOR_ID = 34;
+        public static final int HOOD_MOTOR_ID = 32;
+        public static final int CAN;
 }
